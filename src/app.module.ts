dbeaa5ac40647f84357916fe9b2601980da90f56
Eric Da Cruz <ericdacruz.dev@gmail.com>
2024-04-28 15:30:05 -0300

    feat: Implements authentication with JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MoviesModule, UsersModule],
+  imports: [MoviesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '4ae41c0bf68f560eae341836c89549f0541fee4936bb819a6084aa1380914345',
+};
